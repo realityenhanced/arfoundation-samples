3bb0c9235fe6024322ba4caddf48b4462a4da9be
Nishant Naveen <nishant@Nishants-MBP.home>
2019-02-16 19:35:55 -0800

    Adding Measurement sample

---
+++ Assets/Scripts/Measure/MeasurementController.cs
@@ -0,0 +1,221 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using System.Collections.Generic;
+using TMPro;
+
+/// <summary>
+/// Manages the UX for measuring real world objects.
+/// </summary>
+public class MeasurementController : MonoBehaviour
+{
+    // Settings
+    [Tooltip("The ARSessionOrigin to use for Raycasting")]
+    [SerializeField]
+    private ARSessionOrigin m_arSessionOrigin;
+
+    [Tooltip("Scene object to be used as the cursor")]
+    [SerializeField]
+    private GameObject m_cursor;
+
+    [Tooltip("The prefab to be used as the endpoint of a measurement")]
+    [SerializeField]
+    private GameObject m_endpointPrefab;
+
+    [Tooltip("The material to be used for rendering measurement lines")]
+    [SerializeField]
+    private Material m_lineMaterial;
+
+    [Tooltip("The prefab to be used to display the measured length as text")]
+    [SerializeField]
+    private GameObject m_lengthMeasureText;
+
+    // Private members
+
+    // Dynamic array to store ray cast hits.
+    private const int c_maxRayCastHits = 16; 
+    private static List<ARRaycastHit> s_rayCastHits = new List<ARRaycastHit>(c_maxRayCastHits);
+
+    // A flag to track if the user is drawing a line between two points.
+    private bool m_isMeasurementInProgress = false;
+
+    // The endpoint being used to measure the length from.
+    private GameObject m_currentEndpoint;
+
+    // The line renderer that is used to draw a line from the starting endpoint
+    // to the cursor when a measurement is in progress.
+    // [PERF IMPROVEMENT] Use XRLineRenderer. (https://github.com/Unity-Technologies/XRLineRenderer)
+    private LineRenderer m_currentLineRenderer;
+
+    // The measurement text object that is currently being rendered to.
+    private GameObject m_currentMeasurementText;
+
+    // Text mesh component being used to render the current length.
+    private TextMeshPro m_currentTextField;
+
+    void Start()
+    {
+        // TODO: Validate settings
+    }
+
+    void Update()
+    {
+        if (Camera.current == null)
+        {
+            return;
+        }
+
+        // TODO: Add init game object to scene to display "Move around", and remove it
+        // when AR is available.
+
+        // Shoot a ray from the center of the screen and retrieve the hit points.
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        if (m_arSessionOrigin.Raycast(screenCenter, s_rayCastHits, TrackableType.All)
+            && s_rayCastHits.Count > 0)
+        {
+            // Place the cursor at the first hit point.
+            EnableCursor();
+            m_cursor.transform.SetPositionAndRotation(s_rayCastHits[0].pose.position, Quaternion.identity);
+        }
+        else
+        {
+            // Unable to find any hits in the world, disable the cursor.
+            DisableCursor();
+        }
+        s_rayCastHits.Clear();
+
+        if (m_isMeasurementInProgress)
+        {
+            // If the user is in the middle of measuring, 
+            // update the line and length text.
+            m_currentLineRenderer.SetPosition(1, m_cursor.transform.position);
+            UpdateMeasurementText();
+        }
+
+        // TODO: Add a text in initial screen to "Tap to place endpoint"
+        // On a tap, place an endpoint object at the cursor.
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            // TODO: How to use Anchors to better stabilize the arrows?
+            var endpoint = Instantiate(m_endpointPrefab, m_cursor.transform.position, Quaternion.identity);
+
+            if (!m_isMeasurementInProgress)
+            {
+                // The user has started a measurement operation.
+                // Add a line renderer & text renderer, that tracks the visualization of the measurement,
+                // to the starting endpoint.
+                AddLineRendererToGameObject(endpoint);
+                AddMeasurementTextToGameObject(endpoint);
+                m_currentEndpoint = endpoint;
+
+                StartMeasurementMode();
+            }
+            else
+            {
+                StopMeasurementMode();
+            }
+        }
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (!focus)
+        {
+            // If a measurement is in progress, cleanup on focus loss.
+            CancelCurrentMeasurement();
+        }
+    }
+
+    private void OnDisable()
+    {
+        // If a measurement is in progress, cleanup.
+        CancelCurrentMeasurement();
+    }
+
+    // HELPERS
+    void EnableCursor()
+    {
+        m_cursor.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    void DisableCursor()
+    {
+        m_cursor.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void StartMeasurementMode()
+    {
+        m_isMeasurementInProgress = true;
+    }
+
+    void StopMeasurementMode()
+    {
+        m_isMeasurementInProgress = false;
+        m_currentTextField = null;
+        m_currentLineRenderer = null;
+        m_currentMeasurementText = null;
+        m_currentEndpoint = null;
+    }
+
+    void CancelCurrentMeasurement()
+    {
+        if (m_isMeasurementInProgress)
+        {
+            // If a measurement is in progress, cleanup the endpoint object.
+            Destroy(m_currentEndpoint);
+
+            StopMeasurementMode();
+        }
+    }
+
+    void AddLineRendererToGameObject(GameObject go)
+    {
+        m_currentLineRenderer = go.AddComponent<LineRenderer>();
+        m_currentLineRenderer.material = m_lineMaterial;
+        m_currentLineRenderer.widthMultiplier = 0.0025f;
+
+        // A measurement operation will always be between 2 endpoints.
+        m_currentLineRenderer.positionCount = 2;
+
+        // Use the game object's world space position as the start & end of the
+        // line.
+        m_currentLineRenderer.SetPosition(0, go.transform.position);
+        m_currentLineRenderer.SetPosition(1, go.transform.position);
+    }
+
+    void AddMeasurementTextToGameObject(GameObject go)
+    {
+        var cameraForward = Camera.current.transform.forward;
+        var rotationToFaceCamera = Quaternion.LookRotation(cameraForward);
+        m_currentMeasurementText = Instantiate(m_lengthMeasureText, go.transform.position, rotationToFaceCamera);
+        m_currentMeasurementText.transform.SetParent(go.transform);
+
+        m_currentTextField = m_currentMeasurementText.GetComponentInChildren<TextMeshPro>();
+    }
+
+    // Returns the length to be displayed based on the distance between the cursor
+    // and the endpoint object that is currently set.
+    string GetCurrentDistance()
+    {
+        float distance = Vector3.Distance(m_cursor.transform.position, m_currentEndpoint.transform.position);
+        if (distance < 1.0f)
+        {
+            // Use centimeters, if distance is less than a meter.
+            distance *= 100;
+            return distance.ToString("0.##") + "cm";
+        }
+        return distance.ToString("0.##") + "m";
+    }
+
+    // Updates the value & placement of the text object to be at the center of the measurement line.
+    void UpdateMeasurementText()
+    {
+        var cameraForward = Camera.current.transform.forward;
+        var rotationToFaceCamera = Quaternion.LookRotation(cameraForward);
+        m_currentTextField.SetText(GetCurrentDistance());
+        var center = (m_cursor.transform.position + m_currentEndpoint.transform.position) / 2;
+        m_currentMeasurementText.transform.SetPositionAndRotation(center, rotationToFaceCamera);
+
+        // TODO: Face camera, but be parallel to line
+    }
+}
---
+++ Assets/Scripts/Measure/SessionManager.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+/// <summary>
+/// Manages the UX to be shown when AR tracking is lost or gained.
+/// </summary>
+public class SessionManager : MonoBehaviour
+{
+    // Settings
+    [Tooltip("The ARSession to use")]
+    [SerializeField]
+    private ARSession m_arSession;
+
+    [Tooltip("The animation used to notify the user to move the device")]
+    [SerializeField]
+    private Animator m_moveDeviceAnimation;
+
+    [Tooltip("The animation used to notify the user to start measuring")]
+    [SerializeField]
+    private Animator m_tapToPlaceAnimation;
+
+    [Tooltip("The MeasurementController that will be enabled/disabled on AR tracking gain/loss.")]
+    [SerializeField]
+    private MeasurementController m_measurementController;
+
+    // Constants
+    const string c_fadeOffAnim = "FadeOff";
+    const string c_fadeOnAnim = "FadeOn";
+
+    // Privates
+    // Flags to track the state of device tracking loss/gain UX.
+    private bool m_isMoveDeviceUXActive = true;
+    private bool m_isTapToPlaceUXActive = false;
+
+    private void Start()
+    {
+        //TODO: Check settings
+        // ..
+    }
+
+    private void OnEnable()
+    {
+        ARSubsystemManager.systemStateChanged += ARSystemStateChanged;
+
+        // [ARFoundation Doc Improvement] Documentation for ARSystemState
+        // is not clear whether this is needed. Picked this up from
+        // a forum post.
+        // https://forum.unity.com/threads/checking-for-ar-availability-before-launching-ar-scene.579130/
+        ARSubsystemManager.CreateSubsystems();
+        StartCoroutine(ARSubsystemManager.CheckAvailability());
+    }
+
+    private void OnDisable()
+    {
+        ARSubsystemManager.systemStateChanged -= ARSystemStateChanged;
+    }
+
+    private void ARSystemStateChanged(ARSystemStateChangedEventArgs obj)
+    {
+        HandleARState(obj.state);
+    }
+
+    void Update()
+    {
+        if (m_isTapToPlaceUXActive 
+            && Input.touchCount > 0 
+            && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            // On the first tap after session tracking is lost, remove the 
+            // "Tap to place" UX.
+            RemoveTapToPlaceUX();
+        }
+    }
+
+    // Helpers
+
+    // Helper to handle the current AR state & update UX.
+    private void HandleARState(ARSystemState arState)
+    {
+        switch (arState)
+        {
+            case ARSystemState.SessionTracking:
+                // Tracking has been started. Update the UX & enable measurements.
+                RemoveMoveDeviceUX();
+                StartTapToPlaceUX();
+                m_measurementController.enabled = true;
+                break;
+            case ARSystemState.Unsupported:
+            // Notify user that AR is not supported.
+            //break;
+            case ARSystemState.NeedsInstall:
+            // Notify user that an install is needed for AR support.
+            //break;
+            default:
+                // The AR Session has not started or lost tracking.
+                // Update the UX & Disable measurements.
+                StartMoveDeviceUX();
+                RemoveTapToPlaceUX();
+                m_measurementController.enabled = false;
+                break;
+        }
+    }
+
+    private void StartTapToPlaceUX()
+    {
+        if (!m_isTapToPlaceUXActive)
+        {
+            m_tapToPlaceAnimation.SetTrigger(c_fadeOnAnim);
+            m_isTapToPlaceUXActive = true;
+        }
+    }
+
+    private void RemoveTapToPlaceUX()
+    {
+        if (m_isTapToPlaceUXActive)
+        {
+            m_tapToPlaceAnimation.SetTrigger(c_fadeOffAnim);
+            m_isTapToPlaceUXActive = false;
+        }
+    }
+
+    private void StartMoveDeviceUX()
+    {
+        if (!m_isMoveDeviceUXActive)
+        {
+            m_moveDeviceAnimation.SetTrigger(c_fadeOnAnim);
+            m_isMoveDeviceUXActive = true;
+        }
+    }
+
+    private void RemoveMoveDeviceUX()
+    {
+        if (m_isMoveDeviceUXActive)
+        {
+            m_moveDeviceAnimation.SetTrigger(c_fadeOffAnim);
+            m_isMoveDeviceUXActive = false;
+        }
+    }
+}
